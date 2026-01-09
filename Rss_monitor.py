4bb43742650605ee0d1fa150cd3970869308b04d
adminlove520 <32920883+adminlove520@users.noreply.github.com>
2026-01-09 00:24:59 +0000

    Bump version to V1.0.8

--- Rss_monitor.py
+++ Rss_monitor.py
@@ -8,7 +8,7 @@ from utils.db import init_database
 from utils.notify import push_message
 from utils.rss import check_for_updates
 
-__version__ = "V1.0.7"
+__version__ = "V1.0.8"
 
 # 主函数
 def main():
